656146209701982b30de66be238624fbbbcee5ae
Daniel <117490095+dans2177@users.noreply.github.com>
2023-11-10 22:02:43 -0700

    0.0.2

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="text-center p-10">
+    <div className="text-center p-10 bg-green-900 h-full">
       <h1 className="text-4xl font-bold mb-5">Family Cookbook</h1>
       <button
         className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
